0822afb00e36906e05927db090e24777ac17dd9c
rento yabuki <rento.nky.1203@gmail.com>
2025-04-24 15:22:18 +0900

    simplified the conditional branch

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryabuki <ryabuki@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 16:01:48 by ryabuki           #+#    #+#             */
-/*   Updated: 2025/04/22 16:01:49 by ryabuki          ###   ########.fr       */
+/*   Updated: 2025/04/24 12:06:51 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,21 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc != 1)
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			for (int j = 0; argv[i][j] != '\0'; j++)
-			{
-				argv[i][j] = std::toupper(argv[i][j]);
-			}
-			std::cout << argv[i];
-			if (i != argc - 1)
-				std::cout << " ";
-		}
-		std::cout << std::endl;
-	}
-	else
+	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
 	}
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			argv[i][j] = std::toupper(argv[i][j]);
+		}
+		std::cout << argv[i];
+		if (i != argc - 1)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
 	return (0);
 }
